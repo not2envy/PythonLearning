de8a05d5870e6ba40c007cb49f9a6b13340bbb84
aldo <chrisfundora@gmail.com>
2026-04-22 00:03:53 -0400

    updated the function that reads files for task one

--- Lesson_Two/Task_1_File_Processing.py
+++ Lesson_Two/Task_1_File_Processing.py
@@ -1,1 +1,17 @@
 from Fees import apply_fee #applys the fee to the amount
+
+def safe_process(file_path):
+    results = []
+    try:
+        with open(file_path, 'r') as file:
+            for line in file:
+                try:
+                    numbers = int(line.strip())
+                    filtered_number = [numbers]  if numbers > 10 else []
+                    results.append(filtered_number)
+                except ValueError:
+                    print(f"Skipping invalid line: {line}")
+    except FileNotFoundError:
+        print(f"Error: The file '{file_path}' was not found.")
+    return apply_fee(results)
+
