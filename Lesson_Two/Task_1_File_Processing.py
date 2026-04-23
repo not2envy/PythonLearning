0acfc3a18dc497545762459dc6d742a1f77a37d6
aldo <chrisfundora@gmail.com>
2026-04-23 08:40:59 -0400

    correction on Task One for Week 2

--- Lesson_Two/Task_1_File_Processing.py
+++ Lesson_Two/Task_1_File_Processing.py
@@ -1,17 +1,18 @@
 from Fees import apply_fee #applys the fee to the amount
 
-def safe_process(file_path):
+def safe_process(file_path, threshold):
     results = []
     try:
         with open(file_path, 'r') as file:
             for line in file:
                 try:
                     numbers = int(line.strip())
-                    filtered_number = [numbers]  if numbers > 10 else []
-                    results.append(filtered_number)
+                    if numbers > threshold: #only process numbers above the threshold "Filtering"
+                        results.append(numbers * 1.2) #apply fee to the number
                 except ValueError:
-                    print(f"Skipping invalid line: {line}")
+                    print(f"Skipping invalid line: {line.strip()}")
     except FileNotFoundError:
         print(f"Error: The file '{file_path}' was not found.")
-    return apply_fee(results)
+    return results
 
+print(safe_process("D:\\Documents\\Development\\PythonLearning\\Lesson_Two\\File_Issues.txt", 10))
