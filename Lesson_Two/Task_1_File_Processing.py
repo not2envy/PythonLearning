9cdcfff26ee98693e5a63ee89790ef127d65b47f
aldo <chrisfundora@gmail.com>
2026-04-23 08:50:24 -0400

    updated function to include number of skipped invalid lines

--- Lesson_Two/Task_1_File_Processing.py
+++ Lesson_Two/Task_1_File_Processing.py
@@ -2,6 +2,7 @@ from Fees import apply_fee #applys the fee to the amount
 
 def safe_process(file_path, threshold):
     results = []
+    skipped = 0
     try:
         with open(file_path, 'r') as file:
             for line in file:
@@ -11,8 +12,9 @@ def safe_process(file_path, threshold):
                         results.append(numbers * 1.2) #apply fee to the number
                 except ValueError:
                     print(f"Skipping invalid line: {line.strip()}")
+                    skipped += 1
     except FileNotFoundError:
         print(f"Error: The file '{file_path}' was not found.")
-    return results
+    return results, skipped
 
-print(safe_process("D:\\Documents\\Development\\PythonLearning\\Lesson_Two\\File_Issues.txt", 10))
+print(safe_process("D:\\Documents\\Development\\PythonLearning\\Lesson_Two\\File_Issues.txt", 30))
